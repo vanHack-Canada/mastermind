69903c3dfb0792b477752e7a6b684403810b1363
luciano ortiz silva <lucianoortizsilva@gmail.com>
2016-05-22 14:39:34 -0300

    add options to single player

--- WebContent/resources/angular/app.js
+++ WebContent/resources/angular/app.js
@@ -1,21 +1,45 @@
-angular.module('AppModule', [ 'ngRoute', 'SingleplayerModule' ])
+angular
+		.module('AppModule', [ 'ngRoute', 'ngCookies', 'SingleplayerModule' ])
 
-.controller('AppController',
-		[ '$scope', '$location', function($scope, $location, WebStorage) {
+		.controller(
+				'AppController',
+				[
+						'$scope',
+						'$location',
+						'$cookies',
+						'$cookieStore',
+						function($scope, $location, $cookies, $cookieStore,
+								WebStorage) {
 
-		} ])
+							$scope.getSessionId = function() {
+								return Math
+										.floor((Math.random() * 10000000) + 5009);
+							};
 
-.config([ '$routeProvider', function($routeProvider) {
+							$scope.showBtnSingleplayer = true;
+							$cookieStore.put('userId', $scope.getSessionId());
+							$scope.userId = $cookieStore.get('userId');
+							$scope.colors = [ {} ];
 
-	// SINGLEPLAYER
-	$routeProvider.when('/singleplayer', {
-		templateUrl : 'partials/singleplayer.html',
-		controller : 'SingleplayerController'
-	})
+						} ])
 
-	// DEFAULT
-	$routeProvider.otherwise({
-		redirectTo : 'index.html'
-	});
+		.config([ '$routeProvider', function($routeProvider) {
 
-} ]);
+			// OPTIONS
+			$routeProvider.when('/option', {
+				templateUrl : 'partials/option.html',
+				controller : 'AppController'
+			})
+
+			// SINGLEPLAYER
+			$routeProvider.when('/singleplayer', {
+				templateUrl : 'partials/singleplayer.html',
+				controller : 'SingleplayerController'
+			})
+
+			// DEFAULT
+			$routeProvider.otherwise({
+				redirectTo : 'index.html'
+			});
+
+		} ]);
--- WebContent/resources/angular/singleplayerController.js
+++ WebContent/resources/angular/singleplayerController.js
@@ -2,6 +2,22 @@ angular.module('SingleplayerModule', [ 'AppModule' ])
 
 .controller('SingleplayerController', [ '$scope', function($scope) {
 
-	$scope.hello = 'hellho world';
+	$scope.colorActual = '';
+
+	$scope.colors = [ {
+		blue : 'btn btn-primary',
+		green : 'btn btn-success',
+		yellow : 'btn btn-yellow',
+		gray : 'btn btn-gray',
+		red : 'btn btn-danger',
+		brow : 'btn btn-brow',
+		orange : 'btn btn-warning',
+		pink : 'btn btn-pink'
+	} ];
+
+	$scope.infoColorActual = function(color) {
+		$scope.colorActual = color;
+		alert($scope.colorActual);
+	};
 
 } ]);
